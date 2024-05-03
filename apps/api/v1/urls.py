7bbd4caf9580b09653bb0c036980883f35137b28
kalash-copods <kalash.bhagwat@copods.co>
2024-05-03 11:06:20 +0530

    role views and role APIs

--- apps/api/v1/urls.py
+++ apps/api/v1/urls.py
@@ -1,19 +1,23 @@
 from django.urls import path
+from apps.role.views import GetPostRoles, GetPutDeleteRole
 from apps.user.views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView
-from apps.user import views
 from apps.authentication.views import GoogleSignInView,AccessTokenValidateView, RefreshTokenView
 from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
-    #USER API's
+    #USER APIs
     path('v1/user/', UserListCreateAPIView.as_view(), name='user'),
     path('v1/user/<int:pk>', UserRetrieveUpdateDestroyAPIView.as_view(), name='user-details'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token-obtain-pair-view'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token-refresh'),
     
+    #ROLE APIs
+    path('v1/role/', GetPostRoles.as_view(), name='role'),
+    path('v1/role/<int:pk>', GetPutDeleteRole.as_view(), name='role-details'),
 
-    #Google Sign In API's
+
+    #Google Sign In APIs
     path('v1/authenticate', GoogleSignInView.as_view(), name='googleSignIn'),
     path('v1/validate', AccessTokenValidateView.as_view(), name='validate_token'),
     path('v1/refresh', RefreshTokenView.as_view(), name='refresh_token'),
--- apps/role/views.py
+++ apps/role/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Role
+from .serializers import RoleSerializers
 
-# Create your views here.
+class GetPostRoles(generics.ListCreateAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializers
+
+class GetPutDeleteRole(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializers
+
+    def delete(self, request, *args, **kwargs):
+        return self.get_object().delete() and Response({"message": "Role deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
