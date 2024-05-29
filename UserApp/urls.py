cdb76127773efc10d8d59f7ce3060e3966ccbe17
Anurag Patel <richa@copods.co>
2024-05-29 13:31:34 +0530

    added role and department API

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 
 from UserApp.views.user_views import userList, createUser, user
 from UserApp.views.authentication_views import googleSignIn, adminPanelGoogleSignIn, accessTokenValidate, refreshToken
+from UserApp.views.role_department_views import role, department
 
 urlpatterns = [
     # Authentication APIs URL
@@ -14,4 +15,8 @@ urlpatterns = [
     re_path(r'^users$', userList),
     re_path(r'^createUser$', createUser),
     re_path(r'^user/([0-9a-f-]+)$', user),
+
+    # roles and department APIs URL
+    re_path(r'^roles$', role),
+    re_path(r'^departments$', department),
 ]
