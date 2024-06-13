d0f3a3939594b161e088d72646ee589d0f2eba8b
chandani-copods <chandani.mourya@copods.co>
2024-06-13 14:56:11 +0530

    [update]:created notification model

--- PushNotificationApp/models.py
+++ PushNotificationApp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
 from django.utils.timezone import now
+from django.contrib.postgres.fields import ArrayField
+from LeaveTrackingApp.models import Leave
 from UserApp.models import User  
 
 # Create your models here.
@@ -13,3 +15,21 @@ class FCMToken(models.Model):
 
     def __str__(self):
         return self.fcm_token
+    
+
+class Notification(models.Model):
+    id=models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True, verbose_name="Public identifier")
+    TYPES_CHOICES = [
+        ('Leave Request', 'Leave Request'),
+        ('Upcoming Holidays','Upcoming Holidays')
+        
+    ]
+    types = models.CharField(max_length=100,unique=True, verbose_name="notification types", choices=TYPES_CHOICES)
+    isRead = models.BooleanField(default=False)
+    leaveApplicationId = models.ForeignKey(Leave, on_delete=models.CASCADE, null=True, related_name='leave_request')
+    receivers = ArrayField(models.ForeignKey(User, on_delete=models.CASCADE))
+    title = models.CharField(max_length=100, verbose_name='title')
+    subtitle = models.CharField(max_length=300, verbose_name="subtitle")
+    redireactionUrl = models.CharField(max_length=250, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='request_created_by')
+    created_at = models.DateTimeField(default=now)
