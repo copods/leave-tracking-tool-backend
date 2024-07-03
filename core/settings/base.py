0123f9e1272745c0bcdc7400e15fd5ef611da4e5
Anurag <anurag@copods.co>
2024-07-03 13:37:15 +0530

    ssl update

--- core/settings/base.py
+++ core/settings/base.py
@@ -144,21 +144,16 @@ X_FRAME_OPTIONS = 'DENY'
 
 
 # Ensure that the browser only sends the CSRF token over HTTPS
-CSRF_COOKIE_HTTPONLY = True
+# CSRF_COOKIE_HTTPONLY = True
 
 # Ensure that the browser only sends the session cookie over HTTPS
-SESSION_COOKIE_HTTPONLY = True
+# SESSION_COOKIE_HTTPONLY = True
 
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 SECURE_SSL_REDIRECT = True
 
-
-# Ensures that your site can only be served from the specified domain
-SECURE_HSTS_SECONDS = 3600
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
 
 # ========================================================
 
