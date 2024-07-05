7d4dd84746a0454995f0357e83ae1431dfd3ff66
kalash-copods <kalash.bhagwat@copods.co>
2024-07-05 14:40:40 +0530

    changed allowed_hosts

--- core/settings/base.py
+++ core/settings/base.py
@@ -11,14 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = [
-    "127.0.0.1", 
-    "localhost",
-    "10.0.2.2", 
-    "192.168.1.59", 
-    "192.168.1.104", "0.0.0.0", "159.89.175.231", "43.204.216.178",
-    "43.204.216.123","13.201.76.70"]
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
