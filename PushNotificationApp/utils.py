f2e0048f30f4ce83c7f32f73b4e7534b0c290128
kalash-copods <kalash.bhagwat@copods.co>
2024-07-24 16:04:03 +0530

    refactor: changed pushnotification util and deleted firebase

--- PushNotificationApp/utils.py
+++ PushNotificationApp/utils.py
@@ -4,8 +4,21 @@ from UserApp.models import User
 from UserApp.decorators import user_is_authorized
 import firebase_admin
 from firebase_admin import credentials, messaging
+import os
 
-server_key = 'PushNotificationApp/views/firebase.json'
+server_key = {
+  "type": os.environ.get('TYPE'),
+  "project_id": os.environ.get('PROJECT_ID'),
+  "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
+  "private_key": os.environ.get('PRIVATE_KEY'),
+  "client_email": os.environ.get('CLIENT_EMAIL'),
+  "client_id": os.environ.get("CLIENT_ID"),
+  "auth_uri": os.environ.get("AUTH_URI"),
+  "token_uri": os.environ.get("TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER_x509_CERT_URL"),
+  "client_x509_cert_url": os.environ.get("CLIENT_x509_CERT_URL"),
+  "universe_domain": os.environ.get("UNIVERSE_DOMAIN")
+}
 
 # Initialize Firebase app
 firebase_cred = credentials.Certificate(server_key)
