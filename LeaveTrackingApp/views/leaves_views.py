6d86eed426a2907f8153930cef16377ab2e5b6a1
chandani-copods <chandani.mourya@copods.co>
2024-09-14 21:06:04 +0530

    added marriage leave in leaveType list

--- LeaveTrackingApp/views/leaves_views.py
+++ LeaveTrackingApp/views/leaves_views.py
@@ -24,12 +24,12 @@ import json
 
 
 @csrf_exempt
-@user_is_authorized
+# @user_is_authorized
 def getLeaveTypes(request):
     if request.method=='GET':
-        # leave_types = LeaveType.objects.filter(~Q(rule_set__duration='None') | Q(name='optional_leave') | Q(rule_set__name='miscellaneous_leave'))
+        leave_types = LeaveType.objects.filter(~Q(rule_set__duration='None') | Q(name='optional_leave') | Q(rule_set__name='miscellaneous_leave') | Q(name = 'marriage_leave'))
         # TODO: in phase 2
-        leave_types = LeaveType.objects.all() 
+        # leave_types = LeaveType.objects.all() 
         leave_types_serializer = LeaveTypeSerializer(leave_types, many=True)
         return JsonResponse(leave_types_serializer.data, safe=False)
 
